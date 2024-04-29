8599e56c86e0eb84fff3ba5d5f1dce9947da7a2e
Emma Oyetey <emmaflookis@hotmail.com>
2024-04-29 13:34:30 +0100

    adjusted CaptureGuess so that incorrect input eg two chars, are negated and not passed on into handleGuess

--- src/main/java/org/hangman/CaptureGuess.java
+++ src/main/java/org/hangman/CaptureGuess.java
@@ -8,13 +8,14 @@ public class CaptureGuess {
 
     public CaptureGuess() {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next(); // Read the whole line and trim any leading/trailing whitespace
+        String input;
+        do {
+            input = scanner.next(); // Read the whole line and trim any leading/trailing whitespace
 
-        if (input.length() != 1 || !Character.isLetter(input.charAt(0))) {
-            System.out.println("Oops! Please enter a single letter.");
-            new CaptureGuess(); // Recreate the CaptureGuess object if the input is invalid
-            return;
-        }
+            if (input.length() != 1 || !Character.isLetter(input.charAt(0))) {
+                System.out.println("Oops! Please enter a single letter.");
+            }
+        } while (input.length() != 1 || !Character.isLetter(input.charAt(0)));
 
         this.guess = input.charAt(0);
     }
