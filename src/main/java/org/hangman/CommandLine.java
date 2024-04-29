08ccdca787d0f07ef9699ac45256526c9004d78c
Emma Oyetey <emmaflookis@hotmail.com>
2024-04-29 19:28:15 +0100

    refactored  to use methods from the CommandLine class to handle various prompts and messages during the game; keeping the HangmanGame class focused on game logic while delegating UI-related tasks to the CommandLine class.

--- src/main/java/org/hangman/CommandLine.java
+++ src/main/java/org/hangman/CommandLine.java
@@ -59,14 +59,4 @@ public class CommandLine {
         System.out.println("Boom! You've guessed the word.");
     }
 }
-//    public static void displayLetterMatch(ArrayList<Integer> positions) {
-//        System.out.println("Hooray! The entered letter matches a letter in the random word.");
-//        System.out.println("It occurred at the following positions:");
-//        for (int i = 0; i < positions.size(); i++) {
-//            System.out.print(positions.get(i) + 1); // Adjust position by adding 1
-//            if (i < positions.size() - 1) {
-//                System.out.print(", "); // Add a comma and space if not the last position
-//            }
-//        }
-//        System.out.println();
-//    }
+
